076a55fdc358e2b76fb63bdddb27514df912513d
chantroi <duongchantroi@alwaysdata.net>
2024-06-07 12:52:36 +0000

    delete 2027 files and update 5 files

--- src/app.py
+++ src/app.py
@@ -1,13 +1,58 @@
-from flask import Flask, render_template, request, jsonify, Response
+from flask import Flask, render_template, request, jsonify
 from flask_htmx import HTMX
+from db import AlbumDB, Song
+from ytdl import music
 
 app = Flask(__name__)
 HTMX(app)
+db = AlbumDB()
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    album = request.args.get("album")
+    return render_template("index.html", album=album)
+
+
+@app.route("/list")
+def list_():
+    album = request.args.get("album")
+    pre = None
+    songs = []
+    if album and album != "all":
+        pre = db.list(album)
+    else:
+        pre = db.list()
+    if pre:
+        for song in pre:
+            song = {
+                "name": song.name,
+                "artist": song.artist,
+                "url": song.url,
+                "cover": song.cover,
+                "theme": "#ebd0c2",
+            }
+            songs.append(song)
+    songs = songs.reverse()
+    return jsonify(songs)
+
+
+@app.route("/add")
+def add_song():
+    origin = request.args.get("url")
+    album = request.args.get("album")
+    if not origin:
+        return jsonify(err="no url")
+    info = music(origin)
+    url = info["url"]
+    name = info["title"]
+    artist = info.get("channel")
+    cover = info.get("thumbnail")
+    if album:
+        song = Song(name, artist, url, origin, cover, album)
+    else:
+        song = Song(name, artist, url, origin, cover)
+    return jsonify(db.add(song))
 
 
 if __name__ == "__main__":
---
+++ src/db/__init__.py
@@ -0,0 +1,2 @@
+from .client import AlbumDB
+from .model import Song
