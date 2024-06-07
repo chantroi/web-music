e7ad613f975e314172e7b690849ddfcfc27188e2
chantroi <duongchantroi@alwaysdata.net>
2024-06-07 12:59:04 +0000

    update PythonImportHelper-v2-Completion.json and app.py

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from flask_htmx import HTMX
-from db import AlbumDB, Song
-from ytdl import music
+from .db import AlbumDB, Song
+from .ytdl import music
 
 app = Flask(__name__)
 HTMX(app)
