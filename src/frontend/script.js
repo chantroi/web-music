b85b15f35db5d64d1fae84fa116809b573bb6a9e
chantroi <duongchantroi@alwaysdata.net>
2024-06-09 13:03:41 +0000

    update hosting.c3JjL2Zyb250ZW5k.cache, PythonImportHelper-v2-Completion.json and src/frontend/index.html

---
+++ src/frontend/script.js
@@ -0,0 +1,68 @@
+const Api = "https://collection.serv00.net";
+const urlParams = new URLSearchParams(window.location.search);
+const albumInput = document.querySelector("#album");
+const Album = urlParams.get("a") || "Web Âm Nhạc";
+
+albumInput.value = Album;
+document.title = Album;
+
+const ap = new APlayer({
+  container: document.getElementById("player"),
+  mini: false,
+  autoplay: true,
+  theme: "#FADFA3",
+  preload: "auto",
+  volume: 1,
+  mutex: true,
+  listFolded: true,
+  listMaxHeight: 90,
+  lrcType: 3,
+  audio: [],
+});
+
+function loadPlayer(album) {
+  album.list.forEach((link) => {
+    fetch(`${Api}/get?url=${link}`)
+      .then((response) => response.json())
+      .then((data) => ap.list.add([data]))
+      .catch((err) => console.log(err));
+  });
+}
+
+function loadAlbum() {
+  fetch(`${Api}/album/get`)
+    .then((response) => response.json())
+    .then(
+      (data) =>
+        function () {
+          loadSideNav(data);
+          data.forEach((album) => {
+            if (Album === album.key) {
+              loadPlayer(album);
+            }
+          });
+        }
+    )
+    .catch((err) => console.log(err));
+}
+
+function loadSideNav(albums) {
+  const sideNav = document.querySelector("#mobile-demo");
+  albums.forEach(
+    (album) =>
+      function () {
+        const snItem = `<li><a onclick="loadAlbum(${album})">${album.key}</a></li>`;
+        sideNav.insertAdjacentHTML("beforeend", snItem);
+      }
+  );
+}
+
+document.addEventListener("htmx:afterOnLoad", function (event) {
+  const link = document.querySelector("#search-box").value;
+  fetch(`${Api}/get?url=${link}`)
+    .then((response) => response.json())
+    .then((data) => ap.list.add([data]))
+    .catch((err) => console.log(err));
+
+  document.querySelector("#search-box").value = "";
+});
