292ff68b1368ee4fed87d3002da1f51cdf6fa930
deepnote <tonglanhchua@gmail.com>
2024-06-17 01:30:47 +0000

    update src-html/index.html and script.js

--- src-html/script.js
+++ src-html/script.js
@@ -2,7 +2,7 @@ import { Deta } from "https://cdn.deta.space/js/deta@latest/deta.mjs";
 
 const urlParams = new URLSearchParams(window.location.search);
 const album = urlParams.get("a") || "Web Âm Nhạc";
-const deta = Deta("c0a1xmuzlhz_9ukoxqAYL8nr8w9hPai2kPbyf7qnAaGC");
+const deta = Deta("c0a1xmuzlhz9ukoxqAYL8nr8w9hPai2kPbyf7qnAaGC");
 const base = deta.Base("music");
 const ap = new APlayer({
   container: document.getElementById("player"),
@@ -18,25 +18,42 @@ const ap = new APlayer({
   audio: [],
 });
 
-function loadAlbum() {
-  const res = base.fetch();
-  const items = res.items;
-  items.forEach((item) => {
-    if (album === item.key) {
-      loadPlayer(item.list);
+async function loadAlbum() {
+  try {
+    const res = await base.fetch();
+    console.log("Fetch result:", res);
+
+    if (res && res.items) {
+      const items = res.items;
+      items.forEach((item) => {
+        if (album === item.key) {
+          loadPlayer(item.list);
+        } else {
+          console.log(`Skipping album: ${item.key}`);
+        }
+      });
     } else {
-      console.log(item.key);
+      console.error("No items found in the response");
     }
-  });
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
 }
 
-function loadPlayer(songs) {
-  songs.forEach((link) => {
-    fetch(`https://mydash-webmusic.hf.space/get?url=${link}`)
-      .then((response) => response.json())
-      .then((data) => ap.list.add([data]))
-      .catch((err) => console.log(err));
-  });
+async function loadPlayer(songs) {
+  for (const link of songs) {
+    if (link) {
+      try {
+        const response = await fetch(
+          `https://mydash-webmusic.hf.space/get?url=${link}`
+        );
+        const data = await response.json();
+        ap.list.add([data]);
+      } catch (err) {
+        console.error(`Error loading song from link: ${link}`, err);
+      }
+    }
+  }
 }
 
 loadAlbum();
