24c380928a0712bc6014347a163dfdea82f27ead
chantroi <duongchantroi@alwaysdata.net>
2024-06-28 14:42:38 +0000

    update hosting.c3JjL2Zyb250ZW5k.cache, PythonImportHelper-v2-Completion.json, src/frontend/index.html and script.js

--- src/frontend/script.js
+++ src/frontend/script.js
@@ -1,3 +1,8 @@
+import { Deta } from "https://cdn.deta.space/js/deta@latest/deta.mjs";
+
+const deta = new Deta("c0kEEGmHJte_YjH9AKDzdmP4tm6Zyge3Fme9KyMRNwXB");
+const db = deta.Base("web-music");
+const drive = deta.Drive("web-music");
 const ap = new APlayer({
   container: document.getElementById("player"),
   mini: false,
@@ -13,57 +18,67 @@ const ap = new APlayer({
 });
 
 function loadPlayer(album) {
-  album.forEach((link) => {
-    fetch(
-      `https://667e81b8759f2fa458e0.appwrite.global/?action=music&url=${link}`
-    )
-      .then((response) => response.json())
-      .then((data) => ap.list.add([data]))
-      .catch((err) => console.log(err));
+  album.forEach((data) => {
+    try {
+      const blobMusic = drive.get(`${data.name}.mp3`);
+      const musicUrl = URL.createObjectURL(blobMusic);
+      const song = {
+        name: data.name,
+        artist: data.artist,
+        url: musicUrl,
+        cover: data.cover,
+      };
+      ap.list.add([song]);
+    } catch (err) {
+      console.log(`Error loading song ${data.name}:`, err);
+    }
   });
 }
 
 function loadPlaylist() {
-  fetch("https://webmusic1-se5r0bbh.b4a.run/list")
-    .then((response) => response.json())
-    .then((data) =>
-      data.forEach((album) => {
-        loadPlayer(album.list);
-      })
-    )
-    .catch((err) => console.log(err));
+  try {
+    const response = fetch("https://webmusic1-se5r0bbh.b4a.run/list");
+    const data = await response.json();
+
+    for (const songData of data) {
+      await loadPlayer(songData);
+    }
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-function loadSong() {
+async function loadSong() {
   const link = document.querySelector("#search-box").value;
-  fetch(
-    `https://667e81b8759f2fa458e0.appwrite.global/?action=music&url=${link}`
-  )
-    .then((response) => response.json())
-    .then(
-      (data) =>
-        function () {
-          ap.list.add([data]);
-          fetch(
-            `https://webmusic1-se5r0bbh.b4a.run/add?url=${data.url}&name=${data.name}&artist=${data.artist}&cover=${data.cover}`
-          )
-            .then((response) => response.json())
-            .then((data) => console.log(data))
-            .catch((err) => console.log(err));
-        }
-    )
-    .catch((err) => console.log(err));
+  try {
+    const response = await fetch(
+      `https://667e81b8759f2fa458e0.appwrite.global/?action=music&url=${link}`
+    );
+    const data = await response.json();
+    ap.list.add([data]);
+
+    const addResponse = await fetch(
+      `https://webmusic1-se5r0bbh.b4a.run/add?url=${link}&name=${data.name}&artist=${data.artist}&cover=${data.cover}`
+    );
+    const addData = await addResponse.json();
+    console.log(addData);
+
+    const musicResponse = await fetch(data.url);
+    const musicBlob = await musicResponse.blob();
+    const musicArrayBuffer = await musicBlob.arrayBuffer();
+    const fileName = `${data.name}.mp3`;
+    const putResponse = await drive.put(fileName, {
+      data: new Uint8Array(musicArrayBuffer),
+    });
+    console.log("File uploaded successfully:", putResponse);
+  } catch (err) {
+    console.log(err);
+  }
 
   document.querySelector("#search-box").value = "";
 }
 
-function ytSearch() {
-  const inputText = document.querySelector("#search-box").value;
-  if (!inputText.startsWith("http")) {
-    fetch(
-      `https://667e81b8759f2fa458e0.appwrite.global/?action=search&kw=${inputText}`
-    )
-      .then((response) => response.json())
-      .then((data) => data.forEach((result) => showResult(result)));
-  }
-}
+loadPlaylist();
+document.querySelector("#search-button").addEventListener("click", async () => {
+  await loadSong();
+});
