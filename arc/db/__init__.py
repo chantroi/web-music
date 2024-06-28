5c4adf214e125fb50fa38a330b2058a29201c6d5
chantroi <duongchantroi@alwaysdata.net>
2024-06-28 09:00:19 +0000

    delete 10 files

--- arc/db/__init__.py
+++
@@ -1,2 +0,0 @@
-from .client import AlbumDB
-from .model import Song
--- arc/ytdl.py
+++
@@ -1,8 +0,0 @@
-import yt_dlp
-
-
-def music(video_url):
-    ydl_opts = {"format": "bestaudio/best"}
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info_dict = ydl.extract_info(video_url, download=False)
-        return info_dict
