756fa99c71d979bc415e1d6b73f87734a5f473d2
New Horizon <bosuutap@alwaysdata.net>
2024-06-29 07:08:34 +0000

    update PythonImportHelper-v2-Completion.json and test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from youtube_search import YoutubeSearch
-import json
-
-results = YoutubeSearch("nguoi la oi", max_results=1).to_json()
-
-with open("result.json", "w") as f:
-    json.dump(json.loads(results), f)
