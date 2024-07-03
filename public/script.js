b13b4392f9f6555ff8b61d20e0f02135f37979fb
chantroi <duongchantroi@alwaysdata.net>
2024-07-03 07:53:25 +0000

    update hosting.cHVibGlj.cache and script.js

--- public/script.js
+++ public/script.js
@@ -46,16 +46,7 @@ function loadSong() {
     .then((data) => {
       fetch(
         `https://webmusic1-se5r0bbh.b4a.run/add?url=${link}&name=${data.name}&artist=${data.artist}&cover=${data.cover}`
-      )
-        .then((response) => {
-          response.json();
-        })
-        .then((data) => {
-          console.log(data);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+      );
       ap.list.add([data]);
     })
     .catch((err) => {
